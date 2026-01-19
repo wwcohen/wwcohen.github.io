ce20b3a2c7dda76fa0f6df643dd5a340feb55295
William Cohen <wcohen@macbook-air-331.mynetworksettings.com>
2026-01-19 14:48:37 -0500

    updates and wordcloud examples

--- 10718_s2026/examples/cloud.py
+++ 10718_s2026/examples/cloud.py
@@ -2,27 +2,40 @@ import ast
 import fire
 import math
 import matplotlib.pyplot as plt
+import sys
+
+from collections import Counter
+from nltk.corpus import stopwords
 from wordcloud import WordCloud
 
-
 def load_text(filename):
+    """Text in a file."""
     with open(filename) as fp:
         return fp.read()
 
-def load_freq(filename, inv=False):
-    freq = {}
-    for line in open(filename):
-        (a, b) = ast.literal_eval(line.strip())
-        freq[a] = 1 - math.exp(b) if inv else math.exp(b)
-    return freq
+def word_count(filename):
+    """Count of non-stopwords in a file."""
+    stopword = set(stopwords.words('english'))
+    ctr = Counter([
+        token.lower()
+        for token in load_text(filename).split()
+        if token not in stopword])
+    return ctr
+
+def cmp_counts(filename1, filename2):
+    """Differences in wordcounts."""
+    def score(n1,n2): 
+        return math.log((n1 + 1.0)/(n1 + n2 + 2.0))
+    wc1 = word_count(filename1)
+    wc2 = word_count(filename2)
+    common_words = set(wc1) & set(wc2)
+    return {w: score(wc1[w], wc2[w]) for w in common_words}
 
 def make_freqcloud(freq):
     cloud = WordCloud(
         width=800, 
         height=400, 
         background_color='white',
-        # stop_words is a set of words to ignore (e.g., 'the', 'a', 'is')
-        #stopwords={'a', 'of', 'is', 'the', 'or', 'and', 'This', 'its'}, 
         min_font_size=10
     ).generate_from_frequencies(freq)
     return cloud
@@ -32,8 +45,6 @@ def make_wordcloud(text):
         width=800, 
         height=400, 
         background_color='white',
-        # stop_words is a set of words to ignore (e.g., 'the', 'a', 'is')
-        #stopwords={'a', 'of', 'is', 'the', 'or', 'and', 'This', 'its'}, 
         min_font_size=10
     ).generate(text)
     return cloud
@@ -44,21 +55,39 @@ def plot_cloud(cloud):
     plt.axis("off") # Turn off the axes and tick marks
     plt.show()
 
-def blue():
-    """DailyKos corpus wordcloud.
+def cloud(corpus_filename):
+    """Wordcloud for a corpus.
     """
-    plot_cloud(make_wordcloud(load_text('bluecorpus.txt')))
+    plot_cloud(make_wordcloud(load_text(corpus_filename)))
+
+def cmp_cloud(filename1, filename2):
+    """Wordcloud comparing two corpora.
+    """
+    plot_cloud(make_freqcloud(cmp_counts(filename1, filename2)))
+
+def blue():
+    cloud('bluecorpus.txt')
+
+def red():
+    cloud('redcorpus.txt')
 
 def blue_v_red():
     """DailyKos with words weighted by #(word in blue) / #(word in red + blue)
     """
-    plot_cloud(make_freqcloud(load_freq('blue-red-cmp.txt')))
+    cmp_cloud('bluecorpus.txt', 'redcorpus.txt')
 
 def red_v_blue():
     """DailyKos with words weighted by 1 - (#(word in blue) / #(word in red + blue))
     """
-    plot_cloud(make_freqcloud(load_freq('blue-red-cmp.txt', inv=True)))
+    cmp_cloud('redcorpus.txt', 'bluecorpus.txt')
+
+def jordan_vs_sutton():
+    cmp_cloud('ai-the-revolution.txt', 'bitter-lesson.txt')
+
+def sutton_vs_jordan():
+    cmp_cloud('bitter-lesson.txt', 'ai-the-revolution.txt')
 
 if __name__ == '__main__':
-    fire.Fire()
+    if len(sys.argv) > 1:
+        fire.Fire()
 
